7e33b5ad1c531757780c8c722749aa3c6ad91d3e
terry-u16 <4608868+terry-u16@users.noreply.github.com>
2023-04-29 14:00:59 +0900

    first submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{
+    fmt::Display,
+    io::{stdout, Write},
+};
+
+use grid::{inv, Coordinate, Map2d, ADJACENTS};
+
 use crate::rand::Xoshiro256;
 
 macro_rules! get {
@@ -94,7 +101,335 @@ macro_rules! max {
     }};
 }
 
-fn main() {}
+const N: usize = 14;
+
+#[derive(Debug, Clone)]
+struct Input {
+    n: usize,
+    t: usize,
+    people: Vec<Person>,
+}
+
+impl Input {
+    fn read_input() -> Input {
+        let (n, t) = get!(usize, usize);
+        let mut people = vec![];
+
+        for _ in 0..n {
+            let (r0, c0, r1, c1) = get!(usize, usize, usize, usize);
+            people.push(Person::new(
+                Coordinate::new(r0 - 1, c0 - 1),
+                Coordinate::new(r1 - 1, c1 - 1),
+            ));
+        }
+
+        Input { n, t, people }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Person {
+    home: Coordinate,
+    company: Coordinate,
+}
+
+impl Person {
+    fn new(home: Coordinate, company: Coordinate) -> Self {
+        Self { home, company }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct State {
+    money: i64,
+    collaborator: i64,
+    map: Map2d<[bool; 4]>,
+}
+
+impl State {
+    fn init() -> Self {
+        let map = Map2d::new(vec![[false; 4]; N * N], N);
+        Self {
+            money: 1000000,
+            collaborator: 1,
+            map,
+        }
+    }
+
+    fn calc_construction_cost(&self) -> i64 {
+        for i in 1..100 {
+            if i * i == self.collaborator {
+                return 10_000_000 / i;
+            }
+        }
+
+        (1e7 / (self.collaborator as f64).sqrt()).floor() as i64
+    }
+
+    fn can_construct(&self) -> bool {
+        self.money >= self.calc_construction_cost()
+    }
+
+    fn update(&mut self) {
+        let (money, collaborator) = get!(i64, i64);
+
+        if money == -1 && collaborator == -1 {
+            panic!();
+        }
+
+        self.money = money;
+        self.collaborator = collaborator;
+    }
+}
+
+enum Action {
+    Construct(Coordinate, Coordinate),
+    Collaboration,
+    Money,
+}
+
+impl Action {
+    fn apply(&self, state: &mut State) {
+        if let &Action::Construct(p, q) = self {
+            let mut dir = !0;
+
+            for d in 0..4 {
+                let adj = ADJACENTS[d];
+                let next = p + adj;
+
+                if next == q {
+                    dir = d;
+                    break;
+                }
+            }
+
+            state.map[p][dir] = true;
+            state.map[q][inv(dir)] = true;
+        }
+    }
+}
+
+impl Display for Action {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Action::Construct(p, q) => write!(
+                f,
+                "1 {} {} {} {}",
+                p.row + 1,
+                p.col + 1,
+                q.row + 1,
+                q.col + 1
+            ),
+            Action::Collaboration => write!(f, "2"),
+            Action::Money => write!(f, "3"),
+        }
+    }
+}
+
+fn main() {
+    let input = Input::read_input();
+    let mut state = State::init();
+
+    for turn in 0..input.t {
+        state.update();
+        let action = get_action(&input, &state, turn);
+        action.apply(&mut state);
+        println!("{}", action);
+        stdout().flush().unwrap();
+    }
+}
+
+fn get_action(input: &Input, state: &State, turn: usize) -> Action {
+    if turn >= 350 {
+        return Action::Money;
+    }
+
+    if !state.can_construct() {
+        return Action::Collaboration;
+    }
+
+    let mut current = [Coordinate::new(7, 6); 4];
+
+    for _ in 0..N {
+        for dir in 0..4 {
+            let last = current[dir];
+            current[dir] = current[dir] + ADJACENTS[dir];
+
+            if current[dir].in_map(N) && !state.map[current[dir]][inv(dir)] {
+                return Action::Construct(current[dir], last);
+            }
+        }
+    }
+
+    Action::Collaboration
+}
+
+#[allow(dead_code)]
+pub mod grid {
+    use std::ops::{Div, DivAssign, Mul, MulAssign};
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct Coordinate {
+        pub row: usize,
+        pub col: usize,
+    }
+
+    impl Coordinate {
+        pub const fn new(row: usize, col: usize) -> Self {
+            Self { row, col }
+        }
+
+        pub fn in_map(&self, size: usize) -> bool {
+            self.row < size && self.col < size
+        }
+
+        pub const fn to_index(&self, size: usize) -> usize {
+            self.row * size + self.col
+        }
+
+        pub const fn dist(&self, other: &Self) -> usize {
+            Self::dist_1d(self.row, other.row) + Self::dist_1d(self.col, other.col)
+        }
+
+        const fn dist_1d(x0: usize, x1: usize) -> usize {
+            (x0 as i64 - x1 as i64).abs() as usize
+        }
+    }
+
+    impl Mul<usize> for Coordinate {
+        type Output = Coordinate;
+
+        fn mul(self, rhs: usize) -> Self::Output {
+            Self::new(self.row * rhs, self.col * rhs)
+        }
+    }
+
+    impl MulAssign<usize> for Coordinate {
+        fn mul_assign(&mut self, rhs: usize) {
+            self.row *= rhs;
+            self.col *= rhs;
+        }
+    }
+
+    impl Div<usize> for Coordinate {
+        type Output = Coordinate;
+
+        fn div(self, rhs: usize) -> Self::Output {
+            Self::new(self.row / rhs, self.col / rhs)
+        }
+    }
+
+    impl DivAssign<usize> for Coordinate {
+        fn div_assign(&mut self, rhs: usize) {
+            self.row /= rhs;
+            self.col /= rhs;
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct CoordinateDiff {
+        pub dr: usize,
+        pub dc: usize,
+    }
+
+    impl CoordinateDiff {
+        pub const fn new(dr: usize, dc: usize) -> Self {
+            Self { dr, dc }
+        }
+
+        pub const fn invert(&self) -> Self {
+            Self::new(0usize.wrapping_sub(self.dr), 0usize.wrapping_sub(self.dc))
+        }
+    }
+
+    impl std::ops::Add<CoordinateDiff> for Coordinate {
+        type Output = Coordinate;
+
+        fn add(self, rhs: CoordinateDiff) -> Self::Output {
+            Coordinate::new(self.row.wrapping_add(rhs.dr), self.col.wrapping_add(rhs.dc))
+        }
+    }
+
+    pub const ADJACENTS: [CoordinateDiff; 4] = [
+        CoordinateDiff::new(!0, 0),
+        CoordinateDiff::new(0, 1),
+        CoordinateDiff::new(1, 0),
+        CoordinateDiff::new(0, !0),
+    ];
+
+    pub const DIRECTIONS: [char; 4] = ['U', 'R', 'D', 'L'];
+
+    pub fn inv(dir: usize) -> usize {
+        dir ^ 2
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Map2d<T> {
+        pub width: usize,
+        pub height: usize,
+        map: Vec<T>,
+    }
+
+    impl<T> Map2d<T> {
+        pub fn new(map: Vec<T>, width: usize) -> Self {
+            let height = map.len() / width;
+            debug_assert!(width * height == map.len());
+            Self { width, height, map }
+        }
+    }
+
+    impl<T> std::ops::Index<Coordinate> for Map2d<T> {
+        type Output = T;
+
+        #[inline]
+        fn index(&self, coordinate: Coordinate) -> &Self::Output {
+            &self.map[coordinate.row * self.width + coordinate.col]
+        }
+    }
+
+    impl<T> std::ops::IndexMut<Coordinate> for Map2d<T> {
+        #[inline]
+        fn index_mut(&mut self, coordinate: Coordinate) -> &mut Self::Output {
+            &mut self.map[coordinate.row * self.width + coordinate.col]
+        }
+    }
+
+    impl<T> std::ops::Index<&Coordinate> for Map2d<T> {
+        type Output = T;
+
+        #[inline]
+        fn index(&self, coordinate: &Coordinate) -> &Self::Output {
+            &self.map[coordinate.row * self.width + coordinate.col]
+        }
+    }
+
+    impl<T> std::ops::IndexMut<&Coordinate> for Map2d<T> {
+        #[inline]
+        fn index_mut(&mut self, coordinate: &Coordinate) -> &mut Self::Output {
+            &mut self.map[coordinate.row * self.width + coordinate.col]
+        }
+    }
+
+    impl<T> std::ops::Index<usize> for Map2d<T> {
+        type Output = [T];
+
+        #[inline]
+        fn index(&self, row: usize) -> &Self::Output {
+            let begin = row * self.width;
+            let end = begin + self.width;
+            &self.map[begin..end]
+        }
+    }
+
+    impl<T> std::ops::IndexMut<usize> for Map2d<T> {
+        #[inline]
+        fn index_mut(&mut self, row: usize) -> &mut Self::Output {
+            let begin = row * self.width;
+            let end = begin + self.width;
+            &mut self.map[begin..end]
+        }
+    }
+}
 
 mod rand {
     pub(crate) struct Xoshiro256 {
